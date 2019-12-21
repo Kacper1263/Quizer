c7fa4804c423942c5dd7b103482681910d11d59d
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2019-12-21 19:16:39 +0100

    Password popup in menu

--- AndroidApp/quizer/lib/pages/home.dart
+++ AndroidApp/quizer/lib/pages/home.dart
@@ -9,6 +9,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController ipController = new TextEditingController();
 
+  TextEditingController adminPassController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,12 +51,73 @@ class _HomeState extends State<Home> {
             ),
             OutlineButton(
               child: Text("Admin panel", style: TextStyle(color: Colors.white)),
-              onPressed: () {},
+              onPressed: () {
+                dialog(adminPassController, titleText: "Admin password", descriptionText: "Provide admin password to login", hintText: "E.g. 1423",
+                  onCancel: (){
+                    Fluttertoast.showToast(msg: "Canceled", toastLength: Toast.LENGTH_LONG, backgroundColor: Colors.grey[700], textColor: Colors.white);
+                    Navigator.pop(context);  
+                  },
+                  onSend: () {                    
+                    if(adminPassController.text == "") {
+                      Fluttertoast.showToast(msg: "You need to provide password!", toastLength: Toast.LENGTH_LONG, backgroundColor: Colors.red, textColor: Colors.white);
+                      return;
+                    }
+                    Fluttertoast.showToast(msg: "TODO: Admin panel route/layout and API admin panel", toastLength: Toast.LENGTH_LONG, backgroundColor: Colors.grey[700], textColor: Colors.white);
+                    Navigator.pop(context);  
+                  }
+                );
+              },
               borderSide: BorderSide(color: Colors.grey[400]),
             )
           ],
         ),
       ),
+    );
+  }
+
+
+  void dialog(TextEditingController textCtrl, {titleText, descriptionText, hintText, onCancel, onSend}){    
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return AlertDialog(
+          backgroundColor: Colors.grey[800],
+          title: Text(titleText ,style: TextStyle(color: Colors.white)),            
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(descriptionText ,style: TextStyle(color: Colors.white)),
+                SizedBox(height: 20),
+                TextField(
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                  controller: textCtrl,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey[200])),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey[600])),
+                    border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey[200])),
+                    hintText: hintText,
+                    hintStyle: TextStyle(color: Colors.grey[500]),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancel"),
+              onPressed: onCancel,
+            ),
+            FlatButton(
+              child: Text("Send", style: TextStyle(color: Colors.white),),
+              color: Colors.lightGreen,
+              onPressed: onSend,
+            )
+          ],
+        );
+      }
     );
   }
 }
