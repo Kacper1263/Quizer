dc63fa43462b5f5f9d3bec7955e6deb420866299
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2020-02-02 19:26:21 +0100

    Preparing to versioning (GlobalVAriables class)

---
+++ AndroidApp/quizer/lib/globalVariables.dart
@@ -0,0 +1,7 @@
+class GlobalVariables {
+  //? App version
+  static int appVersionMajor = 1;
+  static int appVersionMinor = 1;
+  static int appVersionPATCH = 0;
+  static String appVersion = "$appVersionMajor.$appVersionMinor.$appVersionPATCH";
+}
